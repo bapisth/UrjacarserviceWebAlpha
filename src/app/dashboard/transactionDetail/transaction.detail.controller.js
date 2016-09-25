ca100e014f9a6406520f0660dfc61af04b2356ce
bapisth <hemendra7011@rediffmail.com>
2016-09-25 19:27:22 +0530

    Tabs Implemented in Customer TransactionDetail.

--- src/app/dashboard/transactionDetail/transaction.detail.controller.js
+++ src/app/dashboard/transactionDetail/transaction.detail.controller.js
@@ -12,6 +12,11 @@
         vm.selectedAgentName = "";
         vm.transactions = [];
         vm.transactionArray = [];
+
+        vm.openTransactionArray = [];
+        vm.progressTransactionArray = [];
+        vm.closedTransactionArray = [];
+
         vm.onClick = onClick;
         vm.changeSelectedItem = changeSelectedItem;
         vm.onCloseTransaction = onCloseTransaction;
@@ -53,6 +58,11 @@
         firebaseDataService.vanWithAgentService.on('value', function (snap) {//Everytime update the select box on any changed done on agent
             vm.transactionArray = [];
 
+            vm.openTransactionArray = [];
+            vm.progressTransactionArray = [];
+            vm.closedTransactionArray = [];
+
+            //Array For Open Transactions
             $firebaseArray(firebaseDataService.Transaction.child(vm.transactionId)).$loaded().then(function (snapshot) {
 
                 vm.transactions = [];
@@ -64,47 +74,173 @@
                         .$loaded().then(function (subChildSnap) {
                         subChildSnap.forEach(function (data, index) {
                             //console.log(subChildSnap);
+                            console.log("Open Tran :"+data.requestStatus);
 
-                            data.carNumber = childSnapshot.$id;
-                            vm.transactions.push(data);
-                            console.log(vm.obj.length);
-                            vm.obj[index] = [];
+                            if (data.requestStatus === "open"){
+                                data.carNumber = childSnapshot.$id;
+                                vm.transactions.push(data);
+                                console.log(vm.obj.length);
 
-                            vm.obj[index]["carNumber"] = childSnapshot.$id;
-                            vm.obj[index]["serviceRequestDate"] = data.serviceRequestDate;
-                            vm.obj[index]["requestStatus"] = data.requestStatus;
+                                vm.obj[index] = [];
 
-                            vm.obj[index]["transactionId"] = data.$id;
-                            vm.obj[index]["addressLine1"] = data.CarPickAddress.addressLine1;
-                            vm.obj[index]["addressLine2"] = data.CarPickAddress.addressLine2;
-                            //{{transaction.CarPickAddress.state}}, {{transaction.CarPickAddress.pin}}
-                            vm.obj[index]["state"] = data.CarPickAddress.state;
-                            vm.obj[index]["pin"] = data.CarPickAddress.pin;
-                            vm.obj[index]["landmark"] = data.CarPickAddress.landmark;
-                            vm.obj[index]["mobileNumber"] = data.CarPickAddress.mobileNumber;
-                            vm.obj[index]["serviceRequestList"] = data.serviceRequestList;
+                                vm.obj[index]["carNumber"] = childSnapshot.$id;
+                                vm.obj[index]["serviceRequestDate"] = data.serviceRequestDate;
+                                vm.obj[index]["requestStatus"] = data.requestStatus;
 
-                            vm.obj[index]["buttonId"] = childSnapshot.$id;
-                            vm.obj[index]["agentNames"] = [];
+                                vm.obj[index]["transactionId"] = data.$id;
+                                vm.obj[index]["addressLine1"] = data.CarPickAddress.addressLine1;
+                                vm.obj[index]["addressLine2"] = data.CarPickAddress.addressLine2;
+                                //{{transaction.CarPickAddress.state}}, {{transaction.CarPickAddress.pin}}
+                                vm.obj[index]["state"] = data.CarPickAddress.state;
+                                vm.obj[index]["pin"] = data.CarPickAddress.pin;
+                                vm.obj[index]["landmark"] = data.CarPickAddress.landmark;
+                                vm.obj[index]["mobileNumber"] = data.CarPickAddress.mobileNumber;
+                                vm.obj[index]["serviceRequestList"] = data.serviceRequestList;
 
-                            var counter = 0;
-                            vm.vanAndAgents.forEach(function (vanAndAgent, idx) {//If the Agent is already assigned then do not show him.
-                                if (!vanAndAgent.isAgentAssignedWithTask) {
-                                    vm.obj[index]["agentNames"][counter] = vanAndAgent;
-                                    counter++;
-                                }
-                            });
+                                vm.obj[index]["buttonId"] = childSnapshot.$id;
+                                vm.obj[index]["agentNames"] = [];
 
-                            vm.selectedItem[index] = [];
-                            vm.obj[index]["agentAssigned"] = data.agentAssigned;
-                            vm.obj[index]["serviceProcessDate"] = data.serviceProcessDate;
-                            vm.obj[index]["serviceCompleteDate"] = data.serviceCompleteDate;
-                            vm.obj[index]["vanNumberAssigned"] = data.vanNumberAssigned;
+                                var counter = 0;
+                                vm.vanAndAgents.forEach(function (vanAndAgent, idx) {//If the Agent is already assigned then do not show him.
+                                    if (!vanAndAgent.isAgentAssignedWithTask) {
+                                        vm.obj[index]["agentNames"][counter] = vanAndAgent;
+                                        counter++;
+                                    }
+                                });
 
-                            vm.transactionArray.push(vm.obj[index]);
-                            console.log(vm.transactionArray);
-                            console.log(vm.transactionArray[index]["serviceRequestDate"])
+                                vm.selectedItem[index] = [];
+                                vm.obj[index]["agentAssigned"] = data.agentAssigned;
+                                vm.obj[index]["serviceProcessDate"] = data.serviceProcessDate;
+                                vm.obj[index]["serviceCompleteDate"] = data.serviceCompleteDate;
+                                vm.obj[index]["vanNumberAssigned"] = data.vanNumberAssigned;
 
+                                vm.openTransactionArray.push(vm.obj[index]);
+                                console.log("Open Transaction Arr :"+vm.openTransactionArray);
+                                //console.log(vm.openTransactionArray[index]["serviceRequestDate"])
+                            }
+
+
+
+                        });
+                    });
+                });
+            });
+
+            //Array For In-Progress Transactions
+            $firebaseArray(firebaseDataService.Transaction.child(vm.transactionId)).$loaded().then(function (snapshot) {
+
+                vm.transactions = [];
+
+                console.log(snapshot.length)
+                snapshot.forEach(function (childSnapshot) {
+                    vm.obj = [];
+                    $firebaseArray(firebaseDataService.Transaction.child(vm.transactionId).child(childSnapshot.$id))
+                        .$loaded().then(function (subChildSnap) {
+                        subChildSnap.forEach(function (data, index) {
+                            //console.log(subChildSnap);
+                            console.log("Progress Tran :"+data.requestStatus);
+
+                            if (data.requestStatus === "progress"){
+                                data.carNumber = childSnapshot.$id;
+                                vm.transactions.push(data);
+                                console.log(vm.obj.length);
+                                vm.obj[index] = [];
+
+                                vm.obj[index]["carNumber"] = childSnapshot.$id;
+                                vm.obj[index]["serviceRequestDate"] = data.serviceRequestDate;
+                                vm.obj[index]["requestStatus"] = data.requestStatus;
+
+                                vm.obj[index]["transactionId"] = data.$id;
+                                vm.obj[index]["addressLine1"] = data.CarPickAddress.addressLine1;
+                                vm.obj[index]["addressLine2"] = data.CarPickAddress.addressLine2;
+                                //{{transaction.CarPickAddress.state}}, {{transaction.CarPickAddress.pin}}
+                                vm.obj[index]["state"] = data.CarPickAddress.state;
+                                vm.obj[index]["pin"] = data.CarPickAddress.pin;
+                                vm.obj[index]["landmark"] = data.CarPickAddress.landmark;
+                                vm.obj[index]["mobileNumber"] = data.CarPickAddress.mobileNumber;
+                                vm.obj[index]["serviceRequestList"] = data.serviceRequestList;
+
+                                vm.obj[index]["buttonId"] = childSnapshot.$id;
+                                vm.obj[index]["agentNames"] = [];
+
+                                var counter = 0;
+                                vm.vanAndAgents.forEach(function (vanAndAgent, idx) {//If the Agent is already assigned then do not show him.
+                                    if (!vanAndAgent.isAgentAssignedWithTask) {
+                                        vm.obj[index]["agentNames"][counter] = vanAndAgent;
+                                        counter++;
+                                    }
+                                });
+
+                                vm.selectedItem[index] = [];
+                                vm.obj[index]["agentAssigned"] = data.agentAssigned;
+                                vm.obj[index]["serviceProcessDate"] = data.serviceProcessDate;
+                                vm.obj[index]["serviceCompleteDate"] = data.serviceCompleteDate;
+                                vm.obj[index]["vanNumberAssigned"] = data.vanNumberAssigned;
+
+                                vm.progressTransactionArray.push(vm.obj[index]);
+                                console.log("Progress Trans Arr :" +vm.progressTransactionArray);
+                                console.log(vm.progressTransactionArray[index]["serviceRequestDate"]);
+                            }
+
+                        });
+                    });
+                });
+            });
+
+            //Array for Closed Transactions
+            $firebaseArray(firebaseDataService.Transaction.child(vm.transactionId)).$loaded().then(function (snapshot) {
+
+                vm.transactions = [];
+
+                console.log(snapshot.length)
+                snapshot.forEach(function (childSnapshot) {
+                    vm.obj = [];
+                    $firebaseArray(firebaseDataService.Transaction.child(vm.transactionId).child(childSnapshot.$id))
+                        .$loaded().then(function (subChildSnap) {
+                        subChildSnap.forEach(function (data, index) {
+                            console.log("Closed Tran :"+data.requestStatus);
+
+                            if (data.requestStatus === "closed"){
+                                data.carNumber = childSnapshot.$id;
+                                vm.transactions.push(data);
+                                console.log(vm.obj.length);
+                                vm.obj[index] = [];
+
+                                vm.obj[index]["carNumber"] = childSnapshot.$id;
+                                vm.obj[index]["serviceRequestDate"] = data.serviceRequestDate;
+                                vm.obj[index]["requestStatus"] = data.requestStatus;
+
+                                vm.obj[index]["transactionId"] = data.$id;
+                                vm.obj[index]["addressLine1"] = data.CarPickAddress.addressLine1;
+                                vm.obj[index]["addressLine2"] = data.CarPickAddress.addressLine2;
+                                //{{transaction.CarPickAddress.state}}, {{transaction.CarPickAddress.pin}}
+                                vm.obj[index]["state"] = data.CarPickAddress.state;
+                                vm.obj[index]["pin"] = data.CarPickAddress.pin;
+                                vm.obj[index]["landmark"] = data.CarPickAddress.landmark;
+                                vm.obj[index]["mobileNumber"] = data.CarPickAddress.mobileNumber;
+                                vm.obj[index]["serviceRequestList"] = data.serviceRequestList;
+
+                                vm.obj[index]["buttonId"] = childSnapshot.$id;
+                                vm.obj[index]["agentNames"] = [];
+
+                                var counter = 0;
+                                vm.vanAndAgents.forEach(function (vanAndAgent, idx) {//If the Agent is already assigned then do not show him.
+                                    if (!vanAndAgent.isAgentAssignedWithTask) {
+                                        vm.obj[index]["agentNames"][counter] = vanAndAgent;
+                                        counter++;
+                                    }
+                                });
+
+                                vm.selectedItem[index] = [];
+                                vm.obj[index]["agentAssigned"] = data.agentAssigned;
+                                vm.obj[index]["serviceProcessDate"] = data.serviceProcessDate;
+                                vm.obj[index]["serviceCompleteDate"] = data.serviceCompleteDate;
+                                vm.obj[index]["vanNumberAssigned"] = data.vanNumberAssigned;
+
+                                vm.closedTransactionArray.push(vm.obj[index]);
+                                console.log(vm.closedTransactionArray);
+                                console.log(vm.closedTransactionArray[index]["serviceRequestDate"]);
+                            }
                         });
                     });
                 });
@@ -118,8 +254,8 @@
             vm.serviceProcessDate[index] = vm.today;
             vm.showServiceProcessDate[index] = true;
 
-            console.log(vm.transactionArray[index]);
-            var mainObj = vm.transactionArray[index];
+            console.log(vm.openTransactionArray[index]);
+            var mainObj = vm.openTransactionArray[index];
             //First Add to Assign the Agent
             var personCarNumber = mainObj.carNumber;
 
@@ -153,8 +289,8 @@
 
             //vm.obj[index]["serviceProcessDate"] = vm.today;
 
-            console.log(vm.transactionArray[index]);
-            var mainObj = vm.transactionArray[index];
+            console.log(vm.progressTransactionArray[index]);
+            var mainObj = vm.progressTransactionArray[index];
 
             mainObj.requestStatus = "closed";
             mainObj.serviceCompleteDate = vm.today;
