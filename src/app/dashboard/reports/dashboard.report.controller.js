8bfd400b97cd0526d725097d00d438aefa79062c
Hemendra Sethi <bapi.bluestack@gmail.com>
2016-12-26 14:34:46 +0530

    Customer Name populated in Report-List.

--- src/app/dashboard/reports/dashboard.report.controller.js
+++ src/app/dashboard/reports/dashboard.report.controller.js
@@ -5,10 +5,10 @@
         .module('app.masterReport')
         .controller('ReportController', ReportController);
 
-    ReportController.$inject = ['$scope', '$location', '$firebaseArray', 'firebaseDataService', 'NgTableParams'];
+    ReportController.$inject = ['$scope', '$location', '$firebaseArray', '$firebaseObject', 'firebaseDataService', 'NgTableParams'];
 
 
-    function ReportController($scope, $location, $firebaseArray, firebaseDataService, NgTableParams) {
+    function ReportController($scope, $location, $firebaseArray, $firebaseObject, firebaseDataService, NgTableParams) {
         var vm = this;
         //Put the contnets in the ng-include='dashboardContent', it is defined in the dashboard.html
         vm.dashboardContent="app/dashboard/reports/masterreport.html";
@@ -27,55 +27,46 @@
         vm.defaultConfigTableParams = null;
         vm.mobileList = null;
         var mobileList = null;
+        var customerName = "";
 
         $firebaseArray(firebaseDataService.Transaction).$loaded().then(function(snapshot){
             var reportMainListData = [];
             snapshot.forEach(function (data, index) {
-                for (var key in data) {
-                    if (key !=="$id" && key !== "$priority"){
-                        vm.reportDataModel = {};
-                        var reportRow = vm.reportDataModel;
-                        if (data.hasOwnProperty(key)) {
-                            var childData  = data[key];
-                            for (var childKey in childData){
-                                var childReportRowData = childData[childKey];
-                                reportRow.agent = childReportRowData.agentAssigned;
-                                reportRow.date = childReportRowData.serviceProcessDate;
-                                reportRow.van = childReportRowData.vanNumberAssigned;
-                                $firebaseArray(firebaseDataService.customer).$loaded().then(function(customerDet){
-                                    reportRow.customerName = customerDet.$getRecord(data.$id).name;
-                                });
-                                //reportRow.customerName = //data.$id; //Customer Id
-                                var amount = 0;
-                                var services = "";
-                                var serviceRqstList = childReportRowData.serviceRequestList;
-                                if (serviceRqstList != undefined){
-                                    serviceRqstList.forEach(function (serviceRqstListData, serviceRqstListIndex) {
-                                        amount += parseInt(serviceRqstListData.vehiclegroup);
-                                        services += serviceRqstListData.desc+ ",";
-                                    });
+                $firebaseObject(firebaseDataService.customer.child(data.$id).child("name")).$loaded().then(function (customerName) {
+                    customerName = customerName.$value;
+                    for (var key in data) {
+                        if (key !=="$id" && key !== "$priority"){
+                            vm.reportDataModel = {};
+                            var reportRow = vm.reportDataModel;
+                            if (data.hasOwnProperty(key)) {
+                                var childData  = data[key];
+                                for (var childKey in childData){
+                                    var childReportRowData = childData[childKey];
+                                    reportRow.agent = childReportRowData.agentAssigned;
+                                    reportRow.date = childReportRowData.serviceProcessDate;
+                                    reportRow.van = childReportRowData.vanNumberAssigned;
+                                    reportRow.customerName = customerName;
+                                    var amount = 0;
+                                    var services = "";
+                                    var serviceRqstList = childReportRowData.serviceRequestList;
+                                    if (serviceRqstList != undefined){
+                                        serviceRqstList.forEach(function (serviceRqstListData, serviceRqstListIndex) {
+                                            amount += parseInt(serviceRqstListData.vehiclegroup);
+                                            services += serviceRqstListData.desc+ ",";
+                                        });
+                                    }
+                                    reportRow.amount = amount;
+                                    reportRow.service = services;
+
                                 }
-                                reportRow.amount = amount;
-                                reportRow.service = services;
-
+                                reportMainListData.push(reportRow);
                             }
-
-                               /* .then(function(snapshot){
-                                snapshot.forEach(function (customerData, customerIndex) {
-                                   if(customerData.$id === data.$id)
-                                        reportRow.customerName = customerData.name;
-                                });
-                            });*/
-                            reportMainListData.push(reportRow);
                         }
                     }
-                }
+                });
             });
 
-            console.log(reportMainListData)
-
             vm.defaultConfigTableParams = new NgTableParams({}, { dataset: reportMainListData});
-            console.log('bahare achhi and table param initialize heijaichi.........');
             vm.defaultConfigTableParams.reload();
 
         });
